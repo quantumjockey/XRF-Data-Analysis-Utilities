008b7d1e1377380c9628899bb6e68310bc72a0d1
quantumjockey <nickyd242@gmail.com>
2014-05-17 23:40:08 -0700

    Prepared existing test classes for spec implementation.

--- Xrf Data Analysis Utilities.Test/Model/xrfPixel_spec.cs
+++ Xrf Data Analysis Utilities.Test/Model/xrfPixel_spec.cs
@@ -1,7 +1,10 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
+﻿///////////////////////////////////////
+#region Namespace Directives
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#endregion
+///////////////////////////////////////
 
 namespace Xrf_Data_Analysis_Utilities.Test.Model
 {
@@ -11,12 +14,21 @@ namespace Xrf_Data_Analysis_Utilities.Test.Model
     [TestClass]
     public class xrfPixel_spec
     {
+        ////////////////////////////////////////
+        #region Constructor (Auto-Generated)
+
+
         public xrfPixel_spec()
         {
             //
             // TODO: Add constructor logic here
             //
         }
+
+        #endregion
+
+        ////////////////////////////////////////
+        #region TestContext Components (Auto-Generated)
 
         private TestContext testContextInstance;
 
@@ -36,34 +48,13 @@ namespace Xrf_Data_Analysis_Utilities.Test.Model
             }
         }
 
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
         #endregion
 
-        [TestMethod]
-        public void TestMethod1()
-        {
-            //
-            // TODO: Add test logic here
-            //
-        }
+        ////////////////////////////////////////
+        #region Unit Tests (Methods)
+
+
+
+        #endregion
     }
 }
--- Xrf Data Analysis Utilities.Test/ViewModel/Workspaces/RampWrapperWorkspaceViewModel_spec.cs
+++ Xrf Data Analysis Utilities.Test/ViewModel/Workspaces/RampWrapperWorkspaceViewModel_spec.cs
@@ -1,7 +1,10 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
+﻿///////////////////////////////////////
+#region Namespace Directives
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#endregion
+///////////////////////////////////////
 
 namespace Xrf_Data_Analysis_Utilities.Test.ViewModel.Workspaces
 {
@@ -11,12 +14,21 @@ namespace Xrf_Data_Analysis_Utilities.Test.ViewModel.Workspaces
     [TestClass]
     public class RampWrapperWorkspaceViewModel_spec
     {
+        ////////////////////////////////////////
+        #region Constructor (Auto-generated)
+
+
         public RampWrapperWorkspaceViewModel_spec()
         {
             //
             // TODO: Add constructor logic here
             //
         }
+
+        #endregion
+
+        ////////////////////////////////////////
+        #region TestContext Components (Auto-Generated)
 
         private TestContext testContextInstance;
 
@@ -36,34 +48,13 @@ namespace Xrf_Data_Analysis_Utilities.Test.ViewModel.Workspaces
             }
         }
 
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
         #endregion
 
-        [TestMethod]
-        public void TestMethod1()
-        {
-            //
-            // TODO: Add test logic here
-            //
-        }
+        ////////////////////////////////////////
+        #region Unit Tests (Methods)
+
+
+
+        #endregion
     }
 }
