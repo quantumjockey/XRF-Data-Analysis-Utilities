b9cc376075a23c0b28c0116db3bf78b69b829780
quantumjockey <nickyd242@gmail.com>
2014-08-17 22:23:51 -0700

    Drafted multiple element layering.

---
+++ XRF Data Analysis Utilities/View/Workspaces/Analysis/MultipleElement/MEAnalysisWorkspace.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XRF_Data_Analysis_Utilities.View.Workspaces.Analysis.MultipleElement
+{
+    /// <summary>
+    /// Interaction logic for MEAnalysisWorkspace.xaml
+    /// </summary>
+    public partial class MEAnalysisWorkspace : UserControl
+    {
+        public MEAnalysisWorkspace()
+        {
+            InitializeComponent();
+        }
+    }
+}
